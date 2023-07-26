5c5f38c65aefbaedf77f280fe40f0b10d5ace965
kchinyama <kaumba@kalichipictures.com>
2023-07-26 13:25:52 -0700

    redo of shell, mem alocation strings files and tools

--- shell.h
+++ shell.h
@@ -13,11 +13,17 @@
 #include <fcntl.h>
 #include <errno.h>
 
-/* ----------Process prototype------------*/
-int read_line(sh_t *);
-int split_line(sh_t *);
-int parse_line(sh_t *);
-int process_cmd(sh_t *);
+/* Global variable */
+extern char **environ;
+
+/* Macros */
+#define BUFSIZE 256
+#define TOKENSIZE 64
+#define PRINT(c) (write(STDOUT_FILENO, c, _strlen(c)))
+#define PROMPT "$ "
+#define SUCCESS (1)
+#define FAIL (-1)
+#define NEUTRAL (0)
 
 /* Struct */
 
@@ -44,8 +50,6 @@ typedef struct sh_data
 	unsigned long int index;
 	char *env;
 } sh_t;
-
-
 /**
  * struct builtin - Manage the builtin functions
  * @cmd: the command line on string form
@@ -58,10 +62,50 @@ typedef struct builtin
 	char *cmd;
 	int (*f)(sh_t *data);
 } blt_t;
+/* ----------Process prototype------------*/
+int read_line(sh_t *);
+int split_line(sh_t *);
+int parse_line(sh_t *);
+int process_cmd(sh_t *);
 
-/* -------------parser prototypes-------------*/
-int is_path_form(sh_t *data);
-void is_short_form(sh_t *data);
-int is_builtin(sh_t *data);
+/* ----------String prototype------------*/
+char *_strdup(char *str);
+char *_strcat(char *first, char *second);
+int _strlen(char *str);
+char *_strchr(char *str, char c);
+int _strcmp(char *s1, char *s2);
+
+/* ----------More String prototype-------*/
+char *_strcpy(char *dest, char *source);
+
+/* ----------Memory prototype------------*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *_memset(char *s, char byt, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int free_data(sh_t *);
+
+/* ----------Tools prototype-------------*/
+void *fill_an_array(void *a, int el, unsigned int len);
+void signal_handler(int signo);
+char *_getenv(char *path_name);
+void index_cmd(sh_t *data);
+void array_rev(char *arr, int len);
+
+/* ----------More tools prototype--------*/
+char *_itoa(unsigned int n);
+int intlen(int num);
+int _atoi(char *c);
+int print_error(sh_t *data);
+int write_history(sh_t *data);
+int _isalpha(int c);
+
+/* -------------Builtins-----------------*/
+int abort_prg(sh_t *data);
+int change_dir(sh_t *data);
+int display_help(sh_t *data);
+int handle_builtin(sh_t *data);
+int check_builtin(sh_t *data);
+
 
 #endif
+
